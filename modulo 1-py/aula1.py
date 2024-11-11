03c1ec95ce65982f3ef9b63c957f554a7534abfc
pecman250 <alicia.batista@pditabira.com>
2024-11-11 19:21:02 -0300

    modulo 1
    atividades modulo 1 py

---
+++ modulo 1-py/aula1.py
@@ -0,0 +1,8 @@
+contador = 0
+while contador !=1:
+
+    N1 = int(input("Digite um numero inteiro:"))
+    N2 = int(input("Digite outro numero inteiro:"))
+    contador = int(input("Digite 1 para parar ou 2 para continuar:"))
+    soma = N1 + N2
+    print("A soma dos número é:", soma)
